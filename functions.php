c8db4ff8677b1abdd4b478d4d54a622c96175379
ughimire <me.umeshuser@gmail.com>
2020-08-25 14:57:42 +0545

    udpate the version

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
  * @since 1.0.0
  */
 
-define('PRAGYA_THEME_VERSION', '0.0.2	');
+define('PRAGYA_THEME_VERSION', '0.0.3	');
 define('PRAGYA_THEME_SETTINGS', 'pragyan');
 define('PRAGYA_THEME_OPTION_PANEL', 'pragyan_theme_option_panel');
 define('PRAGYA_THEME_DIR', trailingslashit(get_template_directory()));
