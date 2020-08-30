67530e92cc99e9b072ec418e074f094cee6e0fa3
ughimire <me.umeshuser@gmail.com>
2020-08-30 13:19:32 +0545

    update

--- template-parts/home/main-content.php
+++ template-parts/home/main-content.php
@@ -1,10 +1,10 @@
 <div class="pragyan-home-page-main-content-area">
 	<div class="container">
 		<div class="row justify-content-center">
-			<div class="col-md-8">
+			<div class="col-md-8 home-page-main-content-sidebar">
 				<?php dynamic_sidebar('pragyan_home_page_main_content_area'); ?>
 			</div>
-			<div class="col-md-4">
+			<div class="col-md-4 home-page-side-content-sidebar">
 				<?php dynamic_sidebar('pragyan_home_page_sidebar'); ?>
 			</div>
 		</div>
